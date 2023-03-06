7d3147ce17910910c8d7457d8541d6fcabca372e
Vishnu Ravi <vishnu.ravi@gmail.com>
2023-03-06 07:54:09 -0500

    Create cloud function to set admin role

---
+++ functions/index.js
@@ -0,0 +1,22 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+
+admin.initializeApp();
+
+exports.updateRole = functions.firestore
+    .document('roles/{userId}')
+    .onUpdate((change, context) => {
+        const newValue = change.after.data();
+
+        const customClaims = {
+            role: newValue.role
+        };
+
+        return admin.auth().setCustomUserClaims(context.params.accessId, customClaims)
+            .then(() => {
+                console.log("Updated role successfully!")
+            })
+            .catch(error => {
+                console.log("Error updating role: " + error)
+            })
+    });
