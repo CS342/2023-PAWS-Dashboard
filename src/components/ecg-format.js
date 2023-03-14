339619cec240804d4df98202368ed289773f0c23
Caitlin Kunchur <crkunchur@gmail.com>
2023-03-13 23:38:23 -0700

    finished ecg graphs

--- src/components/ecg-format.js
+++ src/components/ecg-format.js
@@ -1,11 +1,13 @@
 export default function getVoltageData(decoded_data) {
+
    let start_index = 0
    const components = decoded_data.component
+   const total_samples = decoded_data.component[0].valueQuantity.value
    for (let i = 0; i < components.length; i += 1) {
       if ("valueSampledData" in components[i]) {
       start_index = i
       break;
-      }
+      } 
    }
    // convert voltage string to numbers array
    const numbers_data = (
@@ -15,13 +17,19 @@ export default function getVoltageData(decoded_data) {
    )
       .split(' ')
       .map(Number);
+   console.log(decoded_data.component[start_index].valueSampledData.data + decoded_data.component[start_index + 1].valueSampledData.data + decoded_data.component[start_index + 2].valueSampledData.data)
    const millivolt_data = numbers_data.map((i) => i / 1000);
    const time_voltage_data = []; // map each sample to a time stamp (time interval: 30 s / number of measurements)
    millivolt_data.forEach((sample, index) => {
       time_voltage_data.push({
-         x: (index * 30) / decoded_data.component[0].valueQuantity.value,
+         x: (index * 30) / total_samples,
          y: sample,
       });
    });
-   return time_voltage_data;
+   const first = Math.floor(total_samples / 3)
+   const second = Math.floor(total_samples * (2 / 3))
+   const third = total_samples
+   return [time_voltage_data.slice(0, first), time_voltage_data.slice(first + 1, second), time_voltage_data.slice(second + 1, third)];
+   // return time_voltage_data;
+
 }
