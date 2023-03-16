fcf553f1c8122d9190408c40831956b4ff0b13fc
Caitlin Kunchur <crkunchur@gmail.com>
2023-03-16 14:32:58 -0700

    login style

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,6 +2,7 @@ import { useRef, useState } from 'react';
 import { Form, Button, Card, Alert, Container } from 'react-bootstrap';
 import { useAuth } from '../contexts/AuthContext';
 import { Link, useNavigate } from 'react-router-dom';
+import logo from './paws-logo.svg';
 
 export default function Login() {
    const emailRef = useRef();
@@ -28,13 +29,23 @@ export default function Login() {
 
    return (
       <Container>
-         <Card className="mt-3" style={{width: '25em', margin: '0 auto'}}>
+         <Card className="mt-3" style={{width: '30em', height: '40em', margin: '0 auto'}}>
+        
             <Card.Body>
-               <h2 className="text-center mb-4">Log In</h2>
+           
+            <img
+              alt=""
+              src={logo}
+              width="100"
+              height="100"
+            />
+              <h2 className="text-center mb-4"> </h2>
+               <h2 className="text-center mb-4"> Log In</h2>
+               
                {error && <Alert variant="danger">{error}</Alert>}
                <Form onSubmit={handleSubmit}>
                   <Form.Group id="email">
-                     <Form.Label>Email</Form.Label>
+                     <Form.Label >Email</Form.Label>
                      <Form.Control type="email" ref={emailRef} required />
                   </Form.Group>
                   <Form.Group id="password">
@@ -42,14 +53,16 @@ export default function Login() {
                      <Form.Control type="password" ref={passwordRef} required />
                   </Form.Group>
                   <br />
-                  <Button disabled={loading} className="w-100" type="submit">
+                  <Button style={{background: "salmon" }} disabled={loading} className="w-100" type="submit">
                      Log In
                   </Button>
                </Form>
                <div className="w-100 text-center mt-3">
                   <Link to="/forgot-password">Forgot Password?</Link>
                </div>
+
             </Card.Body>
+       
          </Card>
       </Container>
    );
